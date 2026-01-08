fa4cad2f390f41c66478a940db743546783f1e75
jbergen <jbergen@JBERGEN-T14S.corp.bncollege.com>
2026-01-08 09:12:20 -0500

    commit

---
+++ Object Repository/Site 680 objects/digital purchase.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>digital purchase</name>
+   <tag></tag>
+   <elementGuidId>a4e7f9e0-2900-45af-8f6a-f53b2a788c8a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@type='radio' and @name='radio_MBS_1024802' and @value='MBS_8773020_dg' and @id='digital-MBS_8773020_dg-1']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Site 680 objects/school information continue button.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>school information continue button</name>
+   <tag></tag>
+   <elementGuidId>393c0288-f81a-4793-a078-99f940e2cc46</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button.btn:nth-child(1)</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@class='btn btn-primary']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
